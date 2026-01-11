68ac2da0726be53ddeefff88a55eb98e8813d32d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:47:16 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_identity_provider_facebooks" {
+  description = "All api_management_identity_provider_facebook resources"
+  value       = azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks
+}
+
