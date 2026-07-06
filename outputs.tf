a377c0ee2ac72d2e300f05bdaf4667769df0fb03
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,23 @@
 output "api_management_identity_provider_facebooks" {
   description = "All api_management_identity_provider_facebook resources"
   value       = azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks
+  sensitive   = true
+}
+output "api_management_identity_provider_facebooks_api_management_name" {
+  description = "List of api_management_name values across all api_management_identity_provider_facebooks"
+  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.api_management_name]
+}
+output "api_management_identity_provider_facebooks_app_id" {
+  description = "List of app_id values across all api_management_identity_provider_facebooks"
+  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.app_id]
+}
+output "api_management_identity_provider_facebooks_app_secret" {
+  description = "List of app_secret values across all api_management_identity_provider_facebooks"
+  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.app_secret]
+  sensitive   = true
+}
+output "api_management_identity_provider_facebooks_resource_group_name" {
+  description = "List of resource_group_name values across all api_management_identity_provider_facebooks"
+  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.resource_group_name]
 }
 
