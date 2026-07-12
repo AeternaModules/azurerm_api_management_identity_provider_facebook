c3fcbbb2bf6216d607d4ea7a8a45d8d3a0ecdaf5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "api_management_identity_provider_facebooks" {
-  description = "All api_management_identity_provider_facebook resources"
-  value       = azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks
-  sensitive   = true
-}
 output "api_management_identity_provider_facebooks_api_management_name" {
-  description = "List of api_management_name values across all api_management_identity_provider_facebooks"
-  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_identity_provider_facebooks, keyed the same as var.api_management_identity_provider_facebooks"
+  value       = { for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : k => v.api_management_name }
 }
 output "api_management_identity_provider_facebooks_app_id" {
-  description = "List of app_id values across all api_management_identity_provider_facebooks"
-  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.app_id]
+  description = "Map of app_id values across all api_management_identity_provider_facebooks, keyed the same as var.api_management_identity_provider_facebooks"
+  value       = { for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : k => v.app_id }
 }
 output "api_management_identity_provider_facebooks_app_secret" {
-  description = "List of app_secret values across all api_management_identity_provider_facebooks"
-  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.app_secret]
+  description = "Map of app_secret values across all api_management_identity_provider_facebooks, keyed the same as var.api_management_identity_provider_facebooks"
+  value       = { for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : k => v.app_secret }
   sensitive   = true
 }
 output "api_management_identity_provider_facebooks_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_identity_provider_facebooks"
-  value       = [for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_identity_provider_facebooks, keyed the same as var.api_management_identity_provider_facebooks"
+  value       = { for k, v in azurerm_api_management_identity_provider_facebook.api_management_identity_provider_facebooks : k => v.resource_group_name }
 }
 
